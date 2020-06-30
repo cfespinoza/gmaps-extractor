ee5f60725892b9bffaea99345782fa04f394f1b4
Charles Flores <cflores@stratio.com>
2020-06-30 19:39:17 +0200

    fixes

--- scripts/sql/categorias/insert_categorias.sql
+++ scripts/sql/categorias/insert_categorias.sql
@@ -3,4 +3,8 @@ INSERT INTO premise_type_info (codigo, categoria) VALUES
 (1, 'Restaurantes'),
 (1, 'Cafeterías'),
 (2, 'Gimnasios'),
-(3, 'Peluquerías');
+(3, 'Peluquerías'),
+(4, 'Pub'),
+(5, 'Discoteca'),
+(6, 'Banco Santander');
+(7, 'Banco Bbva');
